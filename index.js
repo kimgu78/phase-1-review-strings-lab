b78a4fbf9cbbcdf220024f0e7c90636be1d8b628
kimgu78 <kimgu78@gmail.com>
2022-02-05 10:38:05 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser = `NODEJS`
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
+let shortGreeting = `Welcome, ${currentUser[0]}!`
